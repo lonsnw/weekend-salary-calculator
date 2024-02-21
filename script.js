7311ad651b36f6c247a8c888280d984e65942b0d
lons <lonsnw@gmail.com>
2024-02-21 14:44:58 -0600

    Uploading first round of work; have set some variables

---
+++ script.js
@@ -0,0 +1,48 @@
+//Weekend challenge to do list: 
+
+//Create inputs for the following:
+//    * first name - "firstNameInput"
+//    * last name - "lastNameInput"
+//    * ID number - "idInput"
+//    * job title - "titleInput"
+//    * annual salary - "annualSalaryInput"
+
+function addEmployee(event) {
+    let firstName = document.querySelector('#firstNameInput').value;
+    let lastName = document.querySelector('#lastNameInput').value;
+    let idNumber = document.querySelector('#idInput').value;
+    let jobTitle = document.querySelector('#titleInput').value;
+    let annualSalary = document.querySelector('#annualSalaryInput').value;
+//add in the table stuff here using the variables i just set;
+//include a delete button that runs a separate function
+//that function will remove a parent element, possibly grandparent
+
+
+
+}
+
+
+
+
+//Create a submit button:
+//    * submit button - 
+        //    <button data-testid="submitButton">
+        //      Submit
+        //    </button>
+
+//The submit button should:
+//    * add the inputs to a table that looks like this: https://github.com/lonsnw/weekend-salary-calculator/raw/main/salary-calc-wireframe.png
+//         * note that every row has a delete button that clears the employee from the dom
+//         * STRETCH GOAL: removing an employee recalculates the monthly cost to pay the employee
+//    * calculates the monthly costs to pay the employees
+//         * note that this will appear in the footer, not in the table
+//    * clear the input fields
+
+//Other tasks: 
+//    * apply a CSS class "over-budget" to the footer when the monthly costs to pay the employees exceeds $20k 
+//    * update the CSS to fit with the theme of the project
+
+
+
+
+
