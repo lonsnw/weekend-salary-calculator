10390b9189fd5a852ac34b7b17e7a888d69575fe
lons <lonsnw@gmail.com>
2024-02-22 08:50:47 -0600

    Saving progress on salary calculations

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //Weekend challenge to do list: 
 
 //Create inputs for the following:
-//    * first name - "firstNameInput"
-//    * last name - "lastNameInput"
-//    * ID number - "idInput"
-//    * job title - "titleInput"
-//    * annual salary - "annualSalaryInput"
+//  ✓  * first name - "firstNameInput"
+//  ✓  * last name - "lastNameInput"
+//  ✓  * ID number - "idInput"
+//  ✓  * job title - "titleInput"
+//  ✓  * annual salary - "annualSalaryInput"
+
+//establishing salaries array outside of function so that it doesn't reset after each input
+let salaries = [];
 
 function addEmployee(event) {
     let firstName = document.querySelector('#firstNameInput').value;
@@ -14,6 +17,8 @@ function addEmployee(event) {
     let jobTitle = document.querySelector('#titleInput').value;
     let annualSalary = document.querySelector('#annualSalaryInput').value;
     let employeeTable = document.querySelector('#employee-data');
+//add the salary to the salaries array
+    salaries.push(annualSalary);
 //add in the table stuff here using the variables i just set;
 //include a delete button that runs a separate function
 //that function will remove a parent element, possibly grandparent
@@ -27,6 +32,14 @@ function addEmployee(event) {
         <td><button onClick="deleteEmployee(event)">Delete</button></td>
         </tr>
         `;
+//incorporate monthly cost calculation into this function so that it calculates every time something is added
+    let annualCost = 0;
+    let monthlyCost = (annualCost / 12);
+    for (let i = 0; i < salaries.length; i ++) {
+        
+    }
+
+    console.log(monthlyCost);
 }
 
 function deleteEmployee(event) {
@@ -35,14 +48,14 @@ function deleteEmployee(event) {
 
 
 //Create a submit button:
-//    * submit button - 
+//  ✓  * submit button - 
         //    <button data-testid="submitButton">
         //      Submit
         //    </button>
 
 //The submit button should:
 //    * add the inputs to a table that looks like this: https://github.com/lonsnw/weekend-salary-calculator/raw/main/salary-calc-wireframe.png
-//         * note that every row has a delete button that clears the employee from the dom
+//  ✓       * note that every row has a delete button that clears the employee from the dom
 //         * STRETCH GOAL: removing an employee recalculates the monthly cost to pay the employee
 //    * calculates the monthly costs to pay the employees
 //         * note that this will appear in the footer, not in the table
